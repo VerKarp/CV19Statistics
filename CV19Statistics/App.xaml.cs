cf4646372ec28475d4b1878472609afab2cac341
VerKarp <vrkrpv@gmail.com>
2022-08-23 19:14:55 +0300

    App folder infrastructure

--- CV19Statistics/App.xaml.cs
+++ CV19Statistics/App.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace CV19Statistics
+﻿namespace CV19Statistics
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+    public partial class App { }
 }
---
+++ CV19Statistics/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace CV19Statistics.Views.Windows
+{
+    public partial class MainWindow
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
