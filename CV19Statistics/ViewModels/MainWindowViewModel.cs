030fedf6913f4c838ad734e35e6ca78e2f2bf452
VerKarp <vrkrpv@gmail.com>
2022-09-01 19:34:53 +0300

    Test vm and command

---
+++ CV19Statistics/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CV19Statistics.ViewModels.Base;
+
+namespace CV19Statistics.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Title : string - Window title
+
+        /// <summary>Window title</summary>
+        private string _title = "CV19 Statistics";
+
+        /// <summary>Window title</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        #endregion
+    }
+}
