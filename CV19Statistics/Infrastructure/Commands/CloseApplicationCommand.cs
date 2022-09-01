bb8dcb75588192222f15ca29249bf824cacec75a
VerKarp <vrkrpv@gmail.com>
2022-09-01 19:33:04 +0300

    Application close command

---
+++ CV19Statistics/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,11 @@
+﻿using CV19Statistics.Infrastructure.Commands.Base;
+
+namespace CV19Statistics.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => App.Current.Shutdown();
+    }
+}
