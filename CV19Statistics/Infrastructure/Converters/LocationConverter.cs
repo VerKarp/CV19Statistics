16201a83929e456f54acc90ce440a103166b3a5e
VerKarp <vrkrpv@gmail.com>
2022-09-02 15:46:27 +0300

    Converter for location

---
+++ CV19Statistics/Infrastructure/Converters/LocationConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace CV19Statistics.Infrastructure.Converters
+{
+    internal class LocationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Point point)) return null;
+            return $"(lat: {point.X}; lon: {point.Y})";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is string str)) return null;
+
+            string[] s = str.Split(";");
+            double lat = double.Parse(s[0].Substring(5));
+            double lon = double.Parse(s[1].Substring(5));
+
+            return new Point(lat, lon);
+        }
+    }
+}
