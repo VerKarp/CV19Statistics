b62fb6299d54eb26590df764999070df6c5e9468
VerKarp <vrkrpv@gmail.com>
2022-09-02 14:54:16 +0300

    Test statistics data service

--- CV19Statistics/ViewModels/MainWindowViewModel.cs
+++ CV19Statistics/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CV19Statistics.Models;
+using CV19Statistics.Services;
+using CV19Statistics.Services.Interfaces;
 using CV19Statistics.ViewModels.Base;
 
 namespace CV19Statistics.ViewModels
@@ -22,5 +25,26 @@ namespace CV19Statistics.ViewModels
         }
 
         #endregion
+
+        #region Countries : List<Country> - Countries
+
+        /// <summary>Countries</summary>
+        private IEnumerable<Country> _countries;
+
+        /// <summary>Countries</summary>
+        public IEnumerable<Country> Countries
+        {
+            get => _countries;
+            set => Set(ref _countries, value);
+        }
+
+        #endregion
+
+        private IDataService _dataService = new DataService();
+
+        public MainWindowViewModel()
+        {
+            Countries = _dataService.GetData();
+        }
     }
 }
