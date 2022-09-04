b808cbf6fd1a4b8903acf4789beb47ee1d46a48c
VerKarp <vrkrpv@gmail.com>
2022-09-05 00:22:30 +0300

    Adding map and converter for map location

---
+++ CV19Statistics/Infrastructure/Converters/MapConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+using MapControl;
+
+namespace CV19Statistics.Infrastructure.Converters
+{
+    internal class MapConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not Point point) return null;
+            return new Location(point.X, point.Y);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not Location location) return null;
+            return new Point(location.Latitude, location.Longitude);
+        }
+    }
+}
