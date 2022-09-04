e6b943c3581a03fba642e215e0cdb5d94337187e
VerKarp <vrkrpv@gmail.com>
2022-09-04 20:34:17 +0300

    Data Context for Main Window

--- CV19Statistics/App.xaml.cs
+++ CV19Statistics/App.xaml.cs
@@ -1,4 +1,21 @@
-﻿namespace CV19Statistics
+﻿using CV19Statistics.Views.Windows;
+using System.Windows;
+using CV19Statistics.ViewModels;
+
+namespace CV19Statistics
 {
-    public partial class App { }
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow()
+            {
+                DataContext = new MainWindowViewModel()
+            };
+
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
 }
