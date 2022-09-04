c75c5202427f633c668ac72362db51467a0b72d9
VerKarp <vrkrpv@gmail.com>
2022-09-04 20:30:20 +0300

    Refactor

--- CV19Statistics/Models/Country.cs
+++ CV19Statistics/Models/Country.cs
@@ -33,16 +33,16 @@ namespace CV19Statistics.Models
             {
                 if (_counts != null) return _counts;
 
-                var points_count = ProvinceCounts.FirstOrDefault()?.Counts?.Count() ?? 0;
-                if (points_count == 0) return Enumerable.Empty<ConfirmedCount>();
+                var pointCount = ProvinceCounts.FirstOrDefault()?.Counts?.Count() ?? 0;
+                if (pointCount == 0) return Enumerable.Empty<ConfirmedCount>();
 
-                var province_points = ProvinceCounts.Select(p => p.Counts.ToArray()).ToArray();
+                var provincePoints = ProvinceCounts.Select(p => p.Counts.ToArray()).ToArray();
 
-                var points = new ConfirmedCount[points_count];
-                foreach (var province in province_points)
-                    for (int i = 0; i < points_count; i++)
+                var points = new ConfirmedCount[pointCount];
+                foreach (var province in provincePoints)
+                    for (int i = 0; i < pointCount; i++)
                     {
-                        if (points[i].Date == default)
+                        if (points[i] is null)
                             points[i] = province[i];
                         else
                             points[i].Count += province[i].Count;
