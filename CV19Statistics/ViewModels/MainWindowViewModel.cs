0c930c68a7653a331a53a6bb6ee154d49ffe78b2
VerKarp <vrkrpv@gmail.com>
2022-09-04 20:32:09 +0300

    Clearning

--- CV19Statistics/ViewModels/MainWindowViewModel.cs
+++ CV19Statistics/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CV19Statistics.Models;
-using CV19Statistics.Services;
+﻿using CV19Statistics.Services;
 using CV19Statistics.Services.Interfaces;
 using CV19Statistics.ViewModels.Base;
 
@@ -12,39 +6,12 @@ namespace CV19Statistics.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        #region Title : string - Window title
-
-        /// <summary>Window title</summary>
-        private string _title = "CV19 Statistics";
-
-        /// <summary>Window title</summary>
-        public string Title
-        {
-            get => _title;
-            set => Set(ref _title, value);
-        }
-
-        #endregion
-
-        #region Countries : List<Country> - Countries
-
-        /// <summary>Countries</summary>
-        private IEnumerable<Country> _countries;
-
-        /// <summary>Countries</summary>
-        public IEnumerable<Country> Countries
-        {
-            get => _countries;
-            set => Set(ref _countries, value);
-        }
-
-        #endregion
-
-        private IDataService _dataService = new DataService();
-
+        public Cv19ConfirmedCountsChartViewModel Cv19ConfirmedCountsChartViewModel { get; set; }
+        
         public MainWindowViewModel()
         {
-            Countries = _dataService.GetData();
+            IDataService dataService = new DataService();
+            Cv19ConfirmedCountsChartViewModel = new Cv19ConfirmedCountsChartViewModel(dataService);
         }
     }
 }
