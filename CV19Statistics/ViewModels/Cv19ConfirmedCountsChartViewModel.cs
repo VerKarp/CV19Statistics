ed0d624e9b2fe03401ebc34589e115392c101898
VerKarp <vrkrpv@gmail.com>
2022-09-06 19:17:22 +0300

    Adding a command to select data and slightly changing the UI

--- CV19Statistics/ViewModels/Cv19ConfirmedCountsChartViewModel.cs
+++ CV19Statistics/ViewModels/Cv19ConfirmedCountsChartViewModel.cs
@@ -46,20 +46,59 @@ namespace CV19Statistics.ViewModels
 
         #endregion
 
+        #region StatisticDataType : Cv19StatisticsDataType - Statistic data type
+
+        /// <summary>Statistic data type</summary>
+        private Cv19StatisticsDataType _statisticsDataType = Cv19StatisticsDataType.Confirmed;
+
+        /// <summary>Statistic data type</summary>
+        public Cv19StatisticsDataType StatisticsDataType
+        {
+            get => _statisticsDataType;
+            set => Set(ref _statisticsDataType, value);
+        }
+
+        #endregion
+
         #region Commands
 
         public ICommand RefreshDataCommand { get; }
-        private void OnRefreshDataCommandExecuted(object p) => Countries = _dataService.GetData();
+        private void OnRefreshDataCommandExecuted(object p) => Countries = _dataService.GetData(_statisticsDataType);
+
+        public ICommand SwitchStatisticDataTypeCommand { get; }
+        private void OnSwitchStatisticDataTypeCommandExecuted(object p)
+        {
+            if (p is null) return;
+
+            switch (p as string)
+            {
+                case "Confirmed":
+                    StatisticsDataType = Cv19StatisticsDataType.Confirmed;
+                    break;
+
+                case "Deaths":
+                    StatisticsDataType = Cv19StatisticsDataType.Deaths;
+                    break;
+
+                case "Recovered":
+                    StatisticsDataType = Cv19StatisticsDataType.Recovered;
+                    break;
+            }
+
+            OnRefreshDataCommandExecuted(this);
+        }
 
         #endregion
 
         public Cv19ConfirmedCountsChartViewModel(IDataService dataService)
         {
             _dataService = dataService;
+            Countries = _dataService.GetData(_statisticsDataType);
 
             #region Commands
 
             RefreshDataCommand = new LambdaCommand(OnRefreshDataCommandExecuted);
+            SwitchStatisticDataTypeCommand = new LambdaCommand(OnSwitchStatisticDataTypeCommandExecuted);
 
             #endregion
         }
